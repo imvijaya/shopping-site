caa7f32a5e8b45c97587455e2ec2a22a3c30460e
KetaEs <eswar-chand.keta@basf.com>
2021-10-21 22:18:50 +0200

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MensPageComponent } from './pages/mens-page/mens-page.component';
+import { WomenPageComponent } from './pages/women-page/women-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PageNotfoundComponent } from './pages/page-notfound/page-notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'men', component:  MensPageComponent },
+  { path: 'women', component:  WomenPageComponent },
+  { path: '', component:  LandingPageComponent },
+  { path: "**", component: PageNotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
